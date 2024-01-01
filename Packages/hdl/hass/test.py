8c9d6e65793ae7d87cfe3202804dbe4d0e9e7299
Edison Sio <65646946+Addidson@users.noreply.github.com>
2024-01-01 22:12:55 +1100

    Modified

--- Packages/hdl/hass/test.py
+++ Packages/hdl/hass/test.py
@@ -9,9 +9,9 @@ import time
 if __name__ == "__main__":
     # endpoint = "api/states/light.corridor_light"
     # url = f"http://192.168.1.18:8123/{endpoint}"
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhY2Y2ZDI4NWI4YzM0MzE5OGViNjFiOTIxOWYxNDYxYyIsImlhdCI6MTcwMzQyNzU2NCwiZXhwIjoyMDE4Nzg3NTY0fQ.baCaGXMBI1msMc6PMYruXDyTt7v9h2NeZFkIzYvs0Rc"
+    token = "<your-token>"
 
-    ip = "192.168.1.18"
+    ip = "<your-ip-address>"
 
     ha = HassComms(token=token, host=ip)
     
@@ -20,9 +20,9 @@ if __name__ == "__main__":
 
     print(devices)
 
-    ac: HassClimate = devices["climate.living_room_a_c"]
-    gaming_light: HassLight = devices["light.gaming_light"]
-    corridor_light: HassLight = devices["light.corridor_light"]
+    # ac: HassClimate = devices["climate.living_room_a_c"]
+    # gaming_light: HassLight = devices["light.gaming_light"]
+    # corridor_light: HassLight = devices["light.corridor_light"]
 
 
     
